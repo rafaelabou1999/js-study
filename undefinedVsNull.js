2a1e27c150ecdb5b135642d801bf4dcdc5419c4e
Rafaela Bourdette <120579642+rafaelabou1999@users.noreply.github.com>
2023-08-24 16:11:12 -0300

    Create undefinedVsNull.js

---
+++ undefinedVsNull.js
@@ -0,0 +1,31 @@
+/**text taken from
+ * --> https://www.scaler.com/topics/javascript/null-and-undefined-in-javascript/
+ */
+
+/**NULL VS UNDEFINED
+ * -------------null
+ * Null in JavaScript means an empty value and
+ *  is also a primitive type in JavaScript. 
+ * The variable which has been assigned as 
+ * null contains no value. Undefined, on the 
+ * other hand, means the variable has been declared, 
+ * but its value has not been assigned.
+ * 
+ * When we assign null as a value to any variable, it 
+ * means that it is empty or blank. 
+ * It is to show that the variable has no value
+ * 
+ * var x = null;
+ * 
+ * ------------undefined
+ * As the name suggests, undefined means "not defined".
+ *  So we declare a variable but do not assign a value to
+ *  it, the variable becomes undefined.
+ * 
+ * When we do not pass an argument for a function
+ *  parameter, the default value is taken as undefined.
+ *  Besides, when a function 
+ * doesn't return a value, it returns undefined.
+ * 
+ * var x;
+ */
